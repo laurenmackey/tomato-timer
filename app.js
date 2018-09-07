90caed6b42fd47018c70084c57192d5b3aa096a2
HDreikorn <hillary@dreikorn.com>
2018-09-07 16:26:36 -0700

    added all the pages to be used and linked them

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
+// Render classical page
+app.get('/classical', function(req, res){
+    res.render('classical');
+});
+
+// Render lofi hip-hop page
+app.get('/lofi', function(req, res){
+    res.render('lofi');
+});
+
+// Render ambient page
+app.get('/ambient', function(req, res){
+    res.render('ambient');
+});
+
+// Render about page
+app.get('/about', function(req, res){
+    res.render('about');
+});
+
 app.listen(app.get('port'), function(){
     console.log('Express started on local host!');
 });
